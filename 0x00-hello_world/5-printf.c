a4e1a1982fa4d423fecf3f0628224878fa4124df
Abdul Razak Kareem <razakkareeem12345@gmail.com>
2023-02-16 13:12:17 -0800

    Made my corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art",\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
         return 0;
 }	
