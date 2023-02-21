82f371be4059471bde07cf6a28c2abe16eb94983
Abdul Razak Kareem <razakkareeem12345@gmail.com>
2023-02-21 09:26:42 -0800

    Wrote the code for the tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar("%c", ch);
+}
+
+_putchar('\n');
+}
+
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This repository is about functions and nested loops
